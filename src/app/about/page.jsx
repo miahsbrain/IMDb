56a1682c6f8df1be4b91e49ed23090f3f141a46f
miahsbrain <160851617+miahsbrain@users.noreply.github.com>
2024-02-25 22:56:42 +0100

    Created About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, tenetur corrupti libero eos nobis, laboriosam magnam, et veniam deserunt praesentium quod ab assumenda temporibus ipsa reprehenderit nemo. Quo, soluta quam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores aperiam culpa explicabo officiis alias obcaecati, eius quisquam recusandae quaerat accusamus?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At odit repellendus, doloremque inventore perspiciatis nobis maiores eos magnam. Eaque minus saepe perferendis facilis rerum aperiam iure voluptatem exercitationem ut quisquam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, recusandae.</p>
+    </div>
+  )
+}
