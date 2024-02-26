cbfaa4344e408d7a4e5602bb0c0a960908d642f8
miahsbrain <160851617+miahsbrain@users.noreply.github.com>
2024-02-26 14:08:26 +0100

    Fixed the styling to fit height

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from "react-icons/fa";
 
 export default function Card({ result={} }) {
   return (
-    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-500 shadow-md rounded-lg sm:border sm:border-slate-500 sm:m-2 transition-shadow transition-duration-200 group'>
+    <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-500 shadow-md rounded-lg sm:border sm:border-slate-500 sm:m-2 transition-shadow transition-duration-200 group h-fit'>
         <Link href={`/movie/${result.id}`}>
             {(result.backdrop_path || result.poster_path) ? (<Image src={`https://image.tmdb.org/t/p/original${result.backdrop_path || result.poster_path}`} width='640' height='300' alt={result.original_title || 'Image not found'} className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity transition-duration-200' placeholder='blur' blurDataURL='/spinner.svg' style={{height:'auto', maxWidth: '100%'}} />) :
             (<div className='rounded-lg w-full lg:w-1/2 text-center'>No Image</div>)}
